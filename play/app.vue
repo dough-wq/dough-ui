b1b5dd5caf7057a6a7d52434fd064c7bd6c56e80
doughs <1572569345@qq.com>
2023-08-07 10:41:21 +0800

    feat: 新增button组件size属性

--- play/app.vue
+++ play/app.vue
@@ -1,10 +1,17 @@
 <template>
   测试
   <d-icon color="red" size="20" class="d-icon-sousuo">Hello</d-icon>
+  <br/>
+  <!-- type -->
   <d-button>按钮</d-button>
-  <d-button type="primary">按钮</d-button>
-  <d-button type="success">按钮</d-button>
-  <d-button type="warning">按钮</d-button>
-  <d-button type="danger">按钮</d-button>
-  <d-button type="info">按钮</d-button>
+  <d-button type="primary">primary</d-button>
+  <d-button type="success">success</d-button>
+  <d-button type="warning">warning</d-button>
+  <d-button type="danger">danger</d-button>
+  <d-button type="info">info</d-button>
+  <br/>
+  <!-- size -->
+  <d-button type="primary" size="large">large</d-button>
+  <d-button type="primary" size="default">default</d-button>
+  <d-button type="primary" size="small">small</d-button>
 </template>
